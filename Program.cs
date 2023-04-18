3a39b9de100a7e3220efe405e8b717d606150898
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2023-04-18 15:44:30 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_124_Practice_Aggregation_Containment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
