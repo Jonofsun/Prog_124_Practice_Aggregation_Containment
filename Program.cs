14e9728626cc3df0fb55533a035dc97fc5f971d2
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2023-04-18 16:25:54 -0700

    Finished
    Updated

--- Program.cs
+++ Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        static List<Student> students = new List<Student>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Instructor John = new Instructor("Jonathan","Reed");
+            SchoolProgram prog124 = new SchoolProgram("Programming", "124", "J124", John);
+            students.Add(new Student("Nami", "Neko", 100, 99));
+            prog124.AddStudent("Nami","Neko",100,99); // Something messed up
+            for (int i = 0; i <  students.Count; i++)
+            {
+                Console.WriteLine(prog124.ToString());
+            }
+            Console.WriteLine(John.ToString());
+            Console.WriteLine(students.ToString());
         }
+        
     }
 }
---
+++ SchoolProgram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_124_Practice_Aggregation_Containment
+{
+    public class SchoolProgram
+    {
+        string _className;
+        string _classNumber;
+        string _roomNumber;
+        Instructor _instructor; // Aggregation
+        List<Student> _students; // Containment
+
+        public SchoolProgram(string className, string classNumber, string roomNumber, Instructor instructor)
+        {
+            _className = className;
+            _classNumber = classNumber;
+            _roomNumber = roomNumber;
+            _instructor = instructor;
+
+        }
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+        public void AddStudent(string firstName, string lastName, int csiGrade, int genEdGrade) // My issue is here due to a null value
+        {
+            if (_students != null)
+            {
+                _students.Add(new Student(firstName, lastName, csiGrade, genEdGrade));
+            }
+            
+        }
+        public override string ToString()
+        {
+            return $"{_className} {_classNumber}: {_roomNumber} - {_instructor} - Number of Students {_students}";
+        }
+    }
+}
